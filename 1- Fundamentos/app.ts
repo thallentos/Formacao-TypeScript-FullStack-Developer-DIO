f2a7ba3bcfbd7f9a18bbc8b028795cf29ef45fa0
thallentos <thallescmarques@gmail.com>
2025-07-31 16:21:04 -0300

    finalizando a parte 2/4

--- 1- Fundamentos/app.ts
+++ 1- Fundamentos/app.ts
@@ -1,3 +1,39 @@
 // Thallentos Banking
 
-// name, accountNumbe
+// name, accountNumber
+// depositar, sacar
+
+abstract class Account {
+  name: string;
+  accountNumber: number;
+  balance: number = 0;
+
+  constructor(name: string, accountNumber: number) {
+    this.name = name;
+    this.accountNumber = accountNumber;
+  }
+
+  deposit = () => {
+    console.log("Deposito realizado com sucesso!");
+  };
+
+  withdraw = () => {
+    console.log("Saque realizado com sucesso!");
+  };
+
+  getBalance = () => {
+    console.log(this.balance);
+  };
+}
+
+class peopleAccount extends Account {
+  doc_id: number;
+  constructor(name: string, accountNumber: number, doc_id: number) {
+    super(name, accountNumber);
+    this.doc_id = doc_id;
+  }
+}
+
+const peopleAccount1 = new peopleAccount("Thalles Coelho Marques", 123456, 123456);
+peopleAccount1.deposit();
+peopleAccount1.getBalance();
