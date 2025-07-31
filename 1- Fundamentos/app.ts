61f366705c2bec055a96c590b7d9f52aa9452d1e
thallentos <thallescmarques@gmail.com>
2025-07-31 06:21:18 -0300

    continuando o curso de ts dev fullstack

--- 1- Fundamentos/app.ts
+++ 1- Fundamentos/app.ts
@@ -1,16 +1,3 @@
-class User {
-  name: string = "Thalles";
-  age: number = 32;
+// Thallentos Banking
 
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-  }
-
-  showName = () => {
-    console.log(this.name);
-  };
-}
-
-const user: User = new User("Thalles", 20);
-user.showName();
+// name, accountNumber
