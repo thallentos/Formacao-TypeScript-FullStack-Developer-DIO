4670985cded514b7956852a712fac931e351706a
thallentos <thallescmarques@gmail.com>
2025-08-07 20:51:13 -0300

    é isso ai

--- 2- Introdução ao React/my-app-ts/src/App.tsx
+++ 2- Introdução ao React/my-app-ts/src/App.tsx
@@ -12,14 +12,31 @@ function App() {
     <ChakraProvider value={defaultSystem}>
       <Box minHeight="100vh" backgroundColor=" #87CEEB" padding="25px">
         <Center>
-          <Box width="75vh" backgroundColor="white" borderRadius="25px" padding="15px" margin="45px">
+          <Box
+            width="75vh"
+            backgroundColor="white"
+            borderRadius="25px"
+            padding="15px"
+            margin="45px"
+          >
             <Center>
               <h1>Faça login</h1>
             </Center>
-            <Input padding='10px' placeholder="Email" />
-            <Input padding='10px' placeholder="Password" />
+            <Input padding="10px" placeholder="Email" />
+            <Input padding="10px" placeholder="Password" />
             <Center>
-              <button onClick={login} style={{ backgroundColor: "#90e9ffff", borderRadius: "25px", padding: "5px 20px 10px", marginTop: "10px", color: "white", cursor: "pointer", width: "100%"}}>
+              <button
+                onClick={login}
+                style={{
+                  backgroundColor: "#90e9ffff",
+                  borderRadius: "25px",
+                  padding: "5px 20px 10px",
+                  marginTop: "10px",
+                  color: "white",
+                  cursor: "pointer",
+                  width: "100%",
+                }}
+              >
                 Entrar
               </button>
             </Center>
