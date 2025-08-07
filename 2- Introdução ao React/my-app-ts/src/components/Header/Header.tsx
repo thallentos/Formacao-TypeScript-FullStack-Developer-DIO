f8775abae7a0d6d7f3e54f210f6ff824323e49ba
thallentos <thallescmarques@gmail.com>
2025-08-07 16:22:22 -0300

    é isso ai

---
+++ 2- Introdução ao React/my-app-ts/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import './Header.css'
+
+export const Header = () => {
+    return (
+        <div className='header'>
+            DIO Bank
+        </div>
+    );
+};
---
+++ 2- Introdução ao React/my-app-ts/src/services/soma.tsx
@@ -0,0 +1,7 @@
+export const soma = (num: number): number => {
+    return num + 1
+}
+
+export const multiplica = (num: number, multiplicador: number): number => {
+    return num * multiplicador
+}
